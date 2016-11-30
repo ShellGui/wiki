3616ed151b83b0de47e938ed2661f51f701d7e99
ek <417@xmlad.com>
2016-11-30 18:19:27 +0800

    Wed Nov 30 18:19:27 CST 2016

---
+++ _static/js/comments.js
@@ -0,0 +1,70 @@
+var apps = {'wan':1,'wifi':2,'lan':3,'adv':4,'status':5,'home':6,'telnetd':7,
+            'firmware':8,'filebrowser':9,'notice':10,'disk':11,'sysinfo':12,'sysusers':13,
+            'jimai':14,'docker':15,'ac-set':16,'dns-cdn':17,'openvpn':18,'ping-watchdog':19,
+            'shadowvpn':20,'shadowsocks':21,'vlan':22,'adbyby-save':23,'wol':24,'bandwidth-usage':25,
+            'bandwidth-distribution':26,'usb-tethering-modem':27,'wifi-spectrum':28,
+            'wifi-client':29,'wire-ap':30,'net-diagnostics':31,'speed-test':32,'mwan3':33,
+            'hosts':34,'conntrack':35,'connlimits':36,'qos-shellgui':37,'lan-net-record':38,
+            'quotas':39,'restriction':40,'firewall-extra':41};
+function getIssue(title){
+  var issue_id = apps[title];
+  var url = 'https://api.github.com/repos/ShellGui/doc_comments/issues/' + issue_id;
+  $.get(url,function(data){
+      makeIssue(data,title);
+      getComments(data.number,title);
+  },'json');
+}
+function makeIssue(issue,title){
+  console.log(arguments);
+  var issueDom = '<header>'
+              +    '<h3 class="comments-title">留言区&nbsp;&nbsp;<sup><span class="badge">' + issue.comments + '</sup></small></h3>'
+              +    '<a href="' + issue.html_url + '#new_comment_field" target="_blank">前往ISSUE发表留言</a>'
+              +  '</header>'
+              +  '<ul class="comments-list list-group"></ul>';
+  $('#' + title + '_comment_container').append(issueDom);
+}
+function getComments(id,title){
+  var url = 'https://api.github.com/repos/ShellGui/doc_comments/issues/' + id + '/comments';
+  $.ajax(url, {
+    headers: {Accept: "application/vnd.github.squirrel-girl-preview.full+json"},
+    dataType: "json",
+    success: function(data){
+      makeComments(data,title);
+    }
+  });
+}
+function makeComments(comments,title){
+  console.log(arguments);
+  for(var i=0; i<comments.length; i++){
+    var comment = comments[i];
+    var date = new Date(comment.created_at);
+    date = date.toLocaleString();
+    var commentDom = '<li class="list-group-item">'
+    +   '<div class="media">'
+    +     '<a class="media-left" href="' + comment.user.html_url + '" target="_blank">'
+    +       '<img src="' + comment.user.avatar_url + '" alt="" width="60" height="60">'
+    +     '</a>'
+    +     '<div class="media-body">'
+    +       '<h4 class="media-heading">' + comment.user.login + '<br><small><span class="create-date">' + date + '</span></small></h4>'
+    +        '<span></span>'
+    +       '<div class="comment-body markdown-body">' + comment.body_html + '</div>'
+    +      '<a class="comment-reactions" href="' + comment.html_url + '" target="_blank">'
+    +          (comment.reactions['heart'] ? '<span class="icon-heart"></span><span class="reaction-count">+' + comment.reactions['heart'] + '</span>' : '')
+    +          (comment.reactions['laugh'] ? '<span class="icon-laugh"></span><span class="reaction-count">+' + comment.reactions['laugh'] + '</span>' : '')
+    +          (comment.reactions['hooray'] ? '<span class="icon-hooray"></span><span class="reaction-count">+' + comment.reactions['hooray'] + '</span>' : '')
+    +          (comment.reactions['confused'] ? '<span class="icon-confused"></span><span class="reaction-count">+' + comment.reactions['confused'] + '</span>' : '')
+    +          (comment.reactions['+1'] ? '<span class="icon-plus"></span><span class="reaction-count">+' + comment.reactions['+1'] + '</span>' : '')
+    +          (comment.reactions['-1'] ? '<span class="icon-minue"></span><span class="reaction-count">+' + comment.reactions['-1'] + '</span>' : '')
+    +      '</a>'
+    +     '</div>'
+    +   '</div>'
+    + '</li>';
+
+    $('#' + title + '_comment_container').find('ul').append(commentDom);
+  }
+}
+
+$('.comment-container').each(function(){
+  var app = $(this).prop('id').replace('_comment_container','');
+  getIssue(app);
+});
